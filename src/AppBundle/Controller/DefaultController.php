0a124c693ef1bd2bebcf6560494487b7272fbfc9
Dmitrij Vasilcenko <d.vasilcenko@gmail.com>
2017-04-09 20:39:15 +0300

    crawler init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DomCrawler\Crawler;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,48 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        //set_time_limit(60);
+        $hasItems = true;
+        $cars = [];
+        while ( $hasItems ) {
+            $pageNumber = 1;
+            $url = "https://autoplius.lt/skelbimai/naudoti-automobiliai?make_id=99&page_nr=" . $pageNumber;
+            $html = $this->getHtml($url);
+            //$html = file_get_contents('test.txt');
+
+            $hasItems = false;
+            $crawler = new Crawler($html);
+            $crawler = $crawler->filter('.item-section');
+
+            foreach ($crawler as $domRow) {
+                $hasItems = true;
+                $row = new Crawler($domRow);
+                $title = $row->filter('.title-list a')->text();
+                $price = trim($row->filter('.price-list')->text());
+                $date = $row->filter('.param-list span[title="Pagaminimo data"]')->text();
+                $car = [$title, $price, $date];
+                $cars[] = $car;
+            }
+
+            $pageNumber++;
+            sleep(1);
+
+            // page 5 not exist so cralwer will stop
+            if ($pageNumber == 5) {
+                break;
+            }
+
+        }
+        return new JsonResponse($cars);
     }
+
+    public function getHtml($url)	{
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $html = curl_exec($curl);
+        curl_close($curl);
+        return $html;
+    }
+
 }
