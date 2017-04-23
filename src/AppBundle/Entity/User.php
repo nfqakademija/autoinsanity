3a1530838994223c79762aa0d4b67d5b7f310291
marttis21 <martynas.sapas@gmail.com>
2017-04-23 18:50:24 +0300

    updated entities with manytomany association for pinning vehicles

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -18,9 +19,19 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="Vehicle", inversedBy="")
+     * @ORM\JoinTable(name="users_vehicles",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="vehicle_id", referencedColumnName="id")}
+     *      )
+     */
+    private $pinnedVehicles;
+
     public function __construct()
     {
         parent::__construct();
+        $this->pinnedVehicles = new ArrayCollection();
     }
 
     public function setEmail($email)
@@ -31,5 +42,31 @@ class User extends BaseUser
         $this->setUsername($email);
 
         return $this;
+    }
+
+    /**
+     * Add pinnedVehicle
+     */
+    public function addPinnedVehicle(Vehicle $pinnedVehicle): User
+    {
+        $this->pinnedVehicles[] = $pinnedVehicle;
+
+        return $this;
+    }
+
+    /**
+     * Remove pinnedVehicle
+     */
+    public function removePinnedVehicle(Vehicle $pinnedVehicle)
+    {
+        $this->pinnedVehicles->removeElement($pinnedVehicle);
+    }
+
+    /**
+     * Get pinnedVehicles
+     */
+    public function getPinnedVehicles(): ArrayCollection
+    {
+        return $this->pinnedVehicles;
     }
 }
