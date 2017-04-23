3695391637fcbbdb6ada83796d5574b23f896009
marttis21 <martynas.sapas@gmail.com>
2017-04-23 21:45:12 +0300

    added ajax for pinning vehicles

--- web/js/search_form_results.js
+++ web/js/search_form_results.js
@@ -1,3 +1,20 @@
+// automatically submit the search form when type of sort is changed
 $('#sort').change(function () {
     this.form.submit();
+});
+
+// send ajax request to pin vehicle
+$('.vehicle-pin-button').click(function () {
+    var button = $(this);
+    $.ajax({
+        url : Routing.generate('pin_vehicle', {id: button.val(), pin_action: 'pin'}),
+        type: 'POST',
+        data : '',
+        success: function(html) {
+            button.html(html);
+        },
+        error: function(html) {
+
+        }
+    });
 });
