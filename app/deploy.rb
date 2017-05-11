a33bcf9b18497e80dd907601a1b9b1370ddf125b
marttis21 <martynas.sapas@gmail.com>
2017-05-11 17:34:00 +0300

    added web/media/cache

--- app/deploy.rb
+++ app/deploy.rb
@@ -9,4 +9,4 @@ set :scm, :rsync
 set :exclude, ['.git', 'node_modules', 'web/app_dev.php']
 
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('var/logs').push('web/img/ads')
+set :linked_dirs, fetch(:linked_dirs, []).push('var/logs').push('web/img/ads').push('web/media/cache')
